35c565bc4f5607f6395dca840f2d2a8191fb8d9f
NemboKid <31885482+NemboKid@users.noreply.github.com>
2019-02-14 21:11:36 +0100

    Add files via upload

---
+++ contracts/SimpleBank.sol
@@ -0,0 +1,101 @@
+pragma solidity ^0.5.0;
+
+
+contract SimpleBank {
+
+    //
+    // State variables
+    //
+
+    /* Fill in the keyword. Hint: We want to protect our users balance from other contracts*/
+    mapping (address => uint) private balances;
+
+    /* Fill in the keyword. We want to create a getter function and allow contracts to be able to see if a user is enrolled.  */
+    mapping (address => bool) public enrolled;
+
+    /* Let's make sure everyone knows who owns the bank. Use the appropriate keyword for this*/
+    address public owner;
+
+    //
+    // Events - publicize actions to external listeners
+    //
+
+    /* Add an argument for this event, an accountAddress */
+    event LogEnrolled(address indexed accountAddress);
+
+    /* Add 2 arguments for this event, an accountAddress and an amount */
+    event LogDepositMade(address indexed accountAddress, uint amount);
+
+    /* Create an event called LogWithdrawal */
+    /* Add 3 arguments for this event, an accountAddress, withdrawAmount and a newBalance */
+    event LogWithdrawal(address indexed accountAddress, uint withdrawAmount, uint newBalance);
+
+
+    //
+    // Functions
+    //
+
+    /* Use the appropriate global variable to get the sender of the transaction */
+    constructor() public {
+        /* Set the owner to the creator of this contract */
+        owner = msg.sender;
+    }
+
+    /// @notice Get balance
+    /// @return The balance of the user
+    // A SPECIAL KEYWORD prevents function from editing state variables;
+    // allows function to run locally/off blockchain
+    function balance() public view returns (uint) {
+        /* Get the balance of the sender of this transaction */
+        return balances[msg.sender];
+    }
+
+    /// @notice Enroll a customer with the bank
+    /// @return The users enrolled status
+    // Emit the appropriate event
+    function enroll() public returns (bool) {
+        enrolled[msg.sender] = true;
+        return enrolled[msg.sender];
+        emit LogEnrolled(msg.sender);
+    }
+
+    /// @notice Deposit ether into bank
+    /// @return The balance of the user after the deposit is made
+    // Add the appropriate keyword so that this function can receive ether
+    // Use the appropriate global variables to get the transaction sender and value
+    // Emit the appropriate event
+    function deposit() public payable returns (uint) {
+        /* Add the amount to the user's balance, call the event associated with a deposit,
+          then return the balance of the user */
+        require(msg.value > 0);
+        balances[msg.sender] += msg.value;
+        emit LogDepositMade(msg.sender, msg.value);
+        return balance();
+    }
+
+    /// @notice Withdraw ether from bank
+    /// @dev This does not return any excess ether sent to it
+    /// @param withdrawAmount amount you want to withdraw
+    /// @return The balance remaining for the user
+    // Emit the appropriate event
+    function withdraw(uint withdrawAmount) public returns (uint accountBalance) {
+        /* If the sender's balance is at least the amount they want to withdraw,
+           Subtract the amount from the sender's balance, and try to send that amount of ether
+           to the user attempting to withdraw.
+           return the user's balance.*/
+           require(balances[msg.sender] >= withdrawAmount);
+           msg.sender.transfer(withdrawAmount);
+           balances[msg.sender] -= withdrawAmount;
+           emit LogWithdrawal (msg.sender, withdrawAmount, balances[msg.sender]);
+           return balances[msg.sender];
+    }
+
+    // Fallback function - Called if other functions don't match call or
+    // sent ether without data
+    // Typically, called when invalid data is sent
+    // Added so ether sent to this contract is reverted if the contract fails
+    // otherwise, the sender's money is transferred to contract
+    function() external payable {
+        revert();
+    }
+}
---
+++ test/simpleBank.test.js
@@ -0,0 +1,70 @@
+/*
+This test file has been updated for Truffle version 5.0. If your tests are failing, make sure that you are
+using Truffle version 5.0. You can check this by running "trufffle version"  in the terminal. If version 5 is not
+installed, you can uninstall the existing version with `npm uninstall -g truffle` and install the latest version (5.0)
+with `npm install -g truffle`.
+*/
+
+var SimpleBank = artifacts.require("./SimpleBank.sol");
+
+contract('SimpleBank', function(accounts) {
+
+  const owner = accounts[0]
+  const alice = accounts[1];
+  const bob = accounts[2];
+  const deposit = web3.utils.toBN(2);
+
+  it("mark addresses as enrolled", async () => {
+    const bank = await SimpleBank.deployed();
+
+    await bank.enroll({from: alice});
+
+    const aliceEnrolled = await bank.enrolled(alice, {from: alice});
+    assert.equal(aliceEnrolled, true, 'enroll balance is incorrect, check balance method or constructor');
+
+    const ownerEnrolled = await bank.enrolled(owner, {from: owner});
+    assert.equal(ownerEnrolled, false, 'only enrolled users should be marked enrolled');
+  });
+
+  it("should deposit correct amount", async () => {
+    const bank = await SimpleBank.deployed();
+
+    await bank.enroll({from: alice});
+    await bank.enroll({from: bob});
+
+    var result = await bank.deposit({from: alice, value: deposit});
+    const balance = await bank.balance({from: alice});
+
+    assert.equal(deposit.toString(), balance, 'deposit amount incorrect, check deposit method');
+
+    const expectedEventResult = {accountAddress: alice, amount: deposit};
+
+    const logAccountAddress = result.logs[0].args.accountAddress;
+    const logDepositAmount = result.logs[0].args.amount.toNumber();
+
+    assert.equal(expectedEventResult.accountAddress, logAccountAddress, "LogDepositMade event accountAddress property not emitted, check deposit method");
+    assert.equal(expectedEventResult.amount, logDepositAmount, "LogDepositMade event amount property not emitted, check deposit method");
+  });
+
+  it("should withdraw correct amount", async () => {
+    const bank = await SimpleBank.deployed();
+    const initialAmount = 0;
+
+	var result = await bank.withdraw(deposit, {from: alice});
+    const balance = await bank.balance({from: alice});
+
+    assert.equal(balance.toString(), initialAmount.toString(), 'balance incorrect after withdrawal, check withdraw method');
+
+    const accountAddress = result.logs[0].args.accountAddress;
+    const newBalance = result.logs[0].args.newBalance.toNumber();
+    const withdrawAmount = result.logs[0].args.withdrawAmount.toNumber();
+
+    const expectedEventResult = {accountAddress: alice, newBalance: initialAmount, withdrawAmount: deposit};
+
+
+    assert.equal(expectedEventResult.accountAddress, accountAddress, "LogWithdrawal event accountAddress property not emitted, check deposit method");
+    assert.equal(expectedEventResult.newBalance, newBalance, "LogWithdrawal event newBalance property not emitted, check deposit method");
+    assert.equal(expectedEventResult.withdrawAmount, withdrawAmount, "LogWithdrawal event withdrawalAmount property not emitted, check deposit method");
+
+  });
+});
